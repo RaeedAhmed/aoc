7fd89dccd659bac4d7e3f7790b61f656e44a5f4e
RaeedAhmed <ruahmed7868@gmail.com>
2022-12-04 20:54:21 -0700

    Unwrapped partial function into combined solution function

--- src/2022-04.py
+++ src/2022-04.py
@@ -1,35 +1,27 @@
+import re
 from functools import partial
 from typing import Callable
 
 from aoc.utils import Session, profiler
 
 
-def section(assignment: str) -> set[int]:
-    bounds = tuple(int(num) for num in assignment.split("-"))
-    return set(range(bounds[0], bounds[1] + 1))
-
-
-def condition(func: Callable, pair: str) -> bool:
-    a, b = (section(x) for x in pair.split(","))
-    return func(a, b)
-
-
-contains = partial(condition, lambda a, b: a.issuperset(b) or b.issuperset(a))
-overlap = partial(condition, lambda a, b: bool(a.intersection(b)))
-
-
 @profiler
-def part(condition: Callable, data: list[str]) -> int:
-    return sum((condition(pair) for pair in data))
+def solution(func: Callable[[set, set], bool], data: str) -> int:
+    sections = re.compile(r"(\d+)-(\d+)\,(\d+)-(\d+)")
+    total = 0
+    for row in data.splitlines():
+        a, b, c, d = map(int, sections.match(row).groups())
+        total += func(set(range(a, b + 1)), set(range(c, d + 1)))
+    return total
 
 
-part1 = partial(part, contains)
-part2 = partial(part, overlap)
+part1 = partial(solution, lambda a, b: a.issuperset(b) or b.issuperset(a))
+part2 = partial(solution, lambda a, b: bool(a.intersection(b)))
 
 
 def main():
     data = Session().load_data()
-    [print(f(data.splitlines())) for f in (part1, part2)]
+    [print(f(data)) for f in (part1, part2)]
 
 
 if __name__ == "__main__":
