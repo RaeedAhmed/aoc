f9a2adc23b72004d74f15c71d2e83e70d78b9b18
RaeedAhmed <ruahmed7868@gmail.com>
2022-12-05 23:27:40 -0700

    Iterators :o

--- src/2022-06.py
+++ src/2022-06.py
@@ -5,16 +5,8 @@ from aoc.utils import Session, profiler
 
 
 @profiler
-def solution(marker_length: int, data: deque[str]) -> int:
-    buffer, count = deque(), marker_length
-    for _ in range(marker_length):
-        buffer.append(data.popleft())
-    while data:
-        if len(set(buffer)) == marker_length:
-            return count
-        buffer.popleft()
-        buffer.append(data.popleft())
-        count += 1
+def solution(n: int, data: str) -> int:
+    return next(i for i in range(n, len(data)) if len(set(data[i - n : i])) == n)
 
 
 part1 = partial(solution, 4)
@@ -23,7 +15,7 @@ part2 = partial(solution, 14)
 
 def main():
     data = Session().load_data()
-    [print(f(deque(data))) for f in (part1, part2)]
+    [print(f(data)) for f in (part1, part2)]
 
 
 if __name__ == "__main__":
